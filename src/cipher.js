2b23a878fdc2ee323de725baf38c4961666b495c
Mariliz G <marilizgonzalez1@gmail.com>
2020-12-18 11:59:52 -0600

    Add all changes

--- src/cipher.js
+++ src/cipher.js
@@ -1,24 +1,34 @@
-const cipher = {    
-let string = document.getElementById("message").value;
-let disp = document.getElementById("displacement").value;
-let newMessage = "";
-//console.log (string);
-for(let i=0; i< string.length; i++) {console.log(string.charCodeAt (i)),
-}
-let offset = string.charCodeAr(i)
-if (offset >= 65&& offset <= 90){ 
-  newMessage += string.fromCharCode(offset-65+disp)%26+65
-}
+const cipher = {
+  encode (offsetNew,oldMessage){
+      let texto; 
+      let newMessage = [];
+          
+      for (let i = 0; i < oldMessage.length; i++){
+          
+          if(oldMessage.charCodeAt(i)>= 65 && oldMessage.charCodeAt(i)<=90){
+              texto = String.fromCharCode((oldMessage.charCodeAt(i)-65+offsetNew)%26+65);
+              newMessage.push(texto)
+            
+          }
+      }
+            return newMessage.join("");
+  },
 
-        
-  // ...
-  /*var */
-// 
-}
+     decode (offsetNew2,oldMessage2){
+        let texto2; 
+        let newMessage2 = [];
+            for (let i = 0; i < oldMessage2.length; i++){
+            
+            if(oldMessage2.charCodeAt(i)>= 65 && oldMessage2.charCodeAt(i)<=90){
+                texto2 = String.fromCharCode((oldMessage2.charCodeAt(i)-65-offsetNew2)%26+65);
+                newMessage2.push(texto2)
+               
+            }
+        }
+        return newMessage2.join("");
+    },
+};
+
 export default cipher;
-/*let string ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-let offset = document.getElementById("displacement").value;
-console.log (string);
 
-for(let i = 0; i < string.length; i++) { console.log(string.charCodeAt (i));
-}
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import cipher from './cipher.js';
+let btn1 = document.getElementById("btnEncode");
+let oldMessage = document.getElementById("originalMessage");
+let offsetNew = document.getElementById("displacement");
+btn1.addEventListener("click", function () {
+    let texto = cipher.encode(parseInt(offsetNew.value), oldMessage.value);
+    
+    document.getElementById("input3").innerHTML = texto;
+});
+let btn2 = document.getElementById("btnDecode");
+let oldMessage2 = document.getElementById("originalMessage");
+let offsetNew2 = document.getElementById("displacement");
+
+btn2.addEventListener("click", function () {
+    let texto2 = cipher.decode(parseInt(offsetNew2.value), oldMessage2.value);
+    
+    document.getElementById("input3").innerHTML = texto2;
+});
+
 
 console.log(cipher);
-console.log("código de Mariliz")
+console.log("código de Mariliz");
